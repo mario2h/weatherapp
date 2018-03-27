071f001ed6580e6560b0d373e42853f032a04c22
Mario Maureira <mario_swin@outlook.com>
2018-03-27 03:31:37 -0300

    se crean componentes

---
+++ src/components/Location.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Location = ({city}) => ( // se aplica object destructuring
+        <div>
+            <h1>{city}</h1>
+        </div>
+    );
+
+export default Location;
