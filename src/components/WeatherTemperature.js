fea26cd4285f96aa36fdc6c361f69488519b5492
Mario Maureira <mario_swin@outlook.com>
2018-04-01 19:23:11 -0300

    se agrega libreria para weather icons, se pasan parámetros de temperatura weatherData a weatherTemperature

--- src/components/WeatherTemperature.js
+++ src/components/WeatherTemperature.js
@@ -1,7 +1,22 @@
 import React from "react";
+import WeatherIcons from "react-weathericons";
 
-const WeatherTemperature = () => (
-    <div><span>12 Cº</span></div>
+const getWeatherIcon = (weatherState) => {
+    switch (weatherState) {
+        case 'cloud':
+            return (<WeatherIcons name="sleet" size="2x"/>);
+            break;
+    
+        default:
+            return (<WeatherIcons name="sleet" size="2x"/>);
+            break;
+    }
+};
+const WeatherTemperature = ({temperature, weatherState}) => (
+    <div>
+        {getWeatherIcon(weatherState)}
+        <span>{`${temperature} Cº`}</span>
+    </div>
 );
 
 export default WeatherTemperature;
