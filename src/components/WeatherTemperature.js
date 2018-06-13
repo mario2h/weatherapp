b1fe18809fd14f0aea671643d671f714f0bb0d0b
Mario Maureira <mario_swin@outlook.com>
2018-06-13 18:47:56 -0400

    se agregan constantes y validaciones de PropTypes

--- src/components/WeatherTemperature.js
+++ src/components/WeatherTemperature.js
@@ -1,22 +1,50 @@
 import React from "react";
 import WeatherIcons from "react-weathericons";
+import PropTypes from "prop-types";
+import { CLOUD, 
+        CLOUDY,
+        SUN,
+        RAIN,
+        SNOW,
+        WINDY } from "./../constants/weathers";
 
-const getWeatherIcon = (weatherState) => {
+const stateToIconName = weatherState => {
     switch (weatherState) {
-        case 'cloud':
-            return (<WeatherIcons name="sleet" size="2x"/>);
+        case CLOUD:
+            return "cloud";
             break;
-    
+        case CLOUDY:
+            return "cloudy";
+            break;
+        case SUN:
+            return "day-sunny";
+            break;
+        case RAIN:
+            return "rain";
+            break;
+        case SNOW:
+            return "snow";
+            break;
         default:
-            return (<WeatherIcons name="sleet" size="2x"/>);
+            return "day-sunny";
             break;
     }
 };
+
+const getWeatherIcon = weatherState => {
+    return (<WeatherIcons name={stateToIconName(weatherState)} size="2x"/>);
+};
+
 const WeatherTemperature = ({temperature, weatherState}) => (
     <div>
         {getWeatherIcon(weatherState)}
         <span>{`${temperature} Cº`}</span>
     </div>
 );
+
+WeatherTemperature.propTypes = {
+    temperature: PropTypes.number.isRequired,
+    weatherState: PropTypes.string,
+};
 
 export default WeatherTemperature;
