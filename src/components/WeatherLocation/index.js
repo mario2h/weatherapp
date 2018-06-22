67adf1d01d049858cfda850646fccc66b66c496e
Mario Maureira <mario_swin@outlook.com>
2018-06-22 00:27:25 -0400

    se implementa propType.Shape, se cambia de functional a class component, se empieza a implementar evento y manejo de state

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, {Component} from "react";
 import Location from "./Location";
 import WeatherData from "./WeatherData";
+import { SUN, WINDY } from "./../../constants/weathers";
 import "./styles.css";
 
-const WeatherLocation = () => (
-    <div className="weatherLocationCont">
-        <Location city={"Santiago"} />
-        <WeatherData />
-    </div>
-);
+const data1 = {
+    temperature: 28,
+    weatherState: SUN,
+    humidity: 10,
+    wind: '15 m/s'
+}
+
+const data2 = {
+    temperature: 17,
+    weatherState: WINDY,
+    humidity: 5,
+    wind: '15 m/s'
+}
+
+class WeatherLocation extends Component {
+    
+    constructor() {
+        super();
+        this.state = {
+            city: "Santiago",
+            data: data1
+        };
+    }
+    
+
+    handleUpdateClick = () => {
+        console.log("actualizado!");
+    }
+    render = () => (
+        <div className="weatherLocationCont">
+            <Location city={this.state.city} />
+            <WeatherData data = {this.state.data} />
+            <button onClick={this.handleUpdateClick}>Actualizar</button>
+        </div>
+    );
+}
 
 export default WeatherLocation;
