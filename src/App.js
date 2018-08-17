8aa31bad9d39dab821ba7c2f8ff68c76f07b72ee
Mario Maureira <mario_swin@outlook.com>
2018-08-17 19:46:40 -0300

    manejo estado inicial app, estilo ClassName ForecastExtended, se crea componente ForecastItem

--- src/App.js
+++ src/App.js
@@ -16,32 +16,39 @@ const cities = [
 ];
 class App extends Component {
 
+  constructor() {
+    super()
+    this.state = {city: null};
+  }
+
   handleSelectedLocation = city => {
+    this.setState({ city });
     console.log(`handleSelectedLocation ${city}`);
   }
   render() {
+    const { city } = this.state;
     return (
       <MuiThemeProvider>
-      <Grid>
-        <Row>
-          <Col xs={12}>
-            <AppBar title="Weather App"/>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12} md={6}>
-            <LocationList cities={cities}
-              onSelectedLocation={this.handleSelectedLocation}></LocationList>
-          </Col>
-          <Col xs={12} md={6}>
-            <Paper zDepth={4}>
-              <div className='detail'>
-                <ForecastExtended></ForecastExtended>
-              </div>
-            </Paper>
-          </Col>
-        </Row>  
-      </Grid>
+        <Grid>
+          <Row>
+            <Col xs={12}>
+              <AppBar title="Weather App"/>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} md={6}>
+              <LocationList cities={cities}
+                onSelectedLocation={this.handleSelectedLocation}></LocationList>
+            </Col>
+            <Col xs={12} md={6}>
+              <Paper zDepth={4}>
+                <div className='detail'>
+                  { city ? <ForecastExtended city={city}></ForecastExtended> : <h1>Seleccione ciudad!</h1> }
+                </div>
+              </Paper>
+            </Col>
+          </Row>
+        </Grid>
       </MuiThemeProvider>
     );
   }
--- src/components/ForecastExtended.jsx
+++ src/components/ForecastExtended.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import ForecastItem from './ForecastItem';
+import './styles.css';
 
 class ForecastExtended extends Component {
   render () {
+    const { city } = this.props;
+
     return (
       <div>
-        Pronóstico Extendido
+        <h2 className='forecast-title'>Pronóstico Extendido para {city}</h2>
+        <ForecastItem></ForecastItem>
       </div>
     )
   }
+}
+
+ForecastExtended.propTypes = {
+  city: PropTypes.string.isRequired,
 }
 
 export default ForecastExtended;
